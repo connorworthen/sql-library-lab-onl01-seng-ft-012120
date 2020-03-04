3c0f0642daea55a793023ae31e467a157962f6da
connor worthen <connorw220@protonmail.com>
2020-03-04 23:45:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,6 +53,6 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN character_books
   ON characters.id = character_books.character_id
   GROUP BY character_books.character_id
-  ORDER BY COUNT(character_books.character_id) DESC LIMIT 1
+  ORDER BY COUNT(character_books.character_id) DESC LIMIT 8
   ;"
 end
